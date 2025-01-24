69f7d3af2b319f8845289908e6708324c7c41b48
Dhruv Naik <dhruviscracked@gmail.com>
2025-01-24 16:16:28 +0530

    Protected routes with clerkMiddleware

--- index.js
+++ index.js
@@ -8,21 +8,24 @@ import communityRoutes from './routes/community.js';
 import plantRoutes from './routes/plants.js';
 import { errorHandler } from './middleware/errorMiddleware.js';
 import { connectDB } from './config/db.js';
+import { requireAuth } from '@clerk/express';
 
 dotenv.config();
 connectDB();
 const app = express();
 app.use(cors());
+
 const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/user', userRoutes);
-app.use('/api/community', communityRoutes);
-app.use('/api/plant', plantRoutes);
-app.use('/api/layout', layoutRoutes);
+app.use('/api/community', requireAuth(), communityRoutes);
+app.use('/api/plant', requireAuth(), plantRoutes);
+app.use('/api/layout', requireAuth(), layoutRoutes);
 app.use('/api/images/upload', imageRoutes);
+
 app.use(errorHandler);
 
 app.listen(port, () => {
